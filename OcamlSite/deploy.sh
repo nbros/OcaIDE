ccebcb7dab1683dbe48b0d7564da9ea8fc3a1eed
nicolas <nicolas.bros@gmail.com>
2011-08-21 17:24:55 +0200

    move projects to subfolder of repo

---
+++ OcamlSite/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SOURCE="/cygdrive/c/Users/Nicolas/workspaces/OcaIDE/OcamlSite"
+TMP=/tmp/OcaIDESite
+
+mkdir -p $TMP
+rm -rf $TMP/*
+
+cd $SOURCE
+cp -r *.html *.jar site.xml plugins/ features/ $TMP
+
+cd $TMP
+
+rm -rf plugins/CVS
+rm -rf features/CVS
+rm -rf plugins/.cvsignore
+rm -rf features/.cvsignore
+
+chmod -R 755 *
+tar czf site.tgz *
+
+
+scp site.tgz nicolas@www.algo-prog.info:
+ssh nicolas@www.algo-prog.info ./deploy
+
