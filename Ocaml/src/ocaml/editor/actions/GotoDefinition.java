adccf9bbe651ccdb13b30e5ba98d0bdf21c3ea6d
trungtq <taquangtrungvn@yahoo.com>
2014-10-07 14:33:57 +0800

    quick jump to next, prev block of code

--- Ocaml/src/ocaml/editor/actions/GotoDefinition.java
+++ Ocaml/src/ocaml/editor/actions/GotoDefinition.java
@@ -4,32 +4,20 @@ import ocaml.OcamlPlugin;
 import ocaml.editors.OcamlEditor;
 import ocaml.editors.OcamlHyperlinkDetector;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.text.FindReplaceDocumentAdapter;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.editors.text.TextEditor;
-import org.eclipse.ui.part.FileEditorInput;
 
-/** This action marks occurrences of the text currently selected in the editor. */
 public class GotoDefinition implements IWorkbenchWindowActionDelegate {
 
 	private IWorkbenchWindow window;
-	
-	private static final String OCAML_GOTO_DEFINITION_ID = "ocaml.marker.gotodefinition";
 	
 	public void run(IAction action) {
 
---
+++ Ocaml/src/ocaml/editor/actions/SelectDownwardOneBlock.java
@@ -0,0 +1,121 @@
+package ocaml.editor.actions;
+
+import ocaml.OcamlPlugin;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class SelectDownwardOneBlock implements IWorkbenchWindowActionDelegate {
+
+	private IWorkbenchWindow window;
+	
+	public void run(IAction action) {
+
+		IWorkbenchPage page = window.getActivePage();
+		
+		if (page == null) {
+			OcamlPlugin.logWarning(GotoDefinition.class.getSimpleName() 
+					+ ": page is null");
+			return;
+		}
+
+		IEditorPart editorPart = page.getActiveEditor();
+		if (editorPart == null) {
+			OcamlPlugin.logError(GotoDefinition.class.getSimpleName() 
+					+ ": editorPart is null");
+			return;
+		}
+		
+		if (!(editorPart instanceof TextEditor)) {
+			OcamlPlugin.logError(GotoDefinition.class.getSimpleName() 
+					+ ": only works on ml and mli files");
+			return;
+		}
+
+		TextEditor editor = (TextEditor) editorPart;
+		IEditorInput editorInput = editor.getEditorInput();
+		IDocument doc = editor.getDocumentProvider().getDocument(editorInput);
+		Control control = (Control)editor.getAdapter(Control.class);
+
+		if (!(control instanceof StyledText)) 
+		{
+			OcamlPlugin.logError(GotoDefinition.class.getSimpleName() 
+					+ ": Cannot get caret position");
+			return;
+		}
+		
+		final StyledText styledText = (StyledText) control;
+		int cursorOffset = styledText.getCaretOffset();
+		
+		TextSelection selection = 
+				(TextSelection) editor.getSelectionProvider().getSelection();
+		int startSelectionOffset;
+		if (selection.getOffset() < cursorOffset)
+			startSelectionOffset = selection.getOffset();
+		else
+			startSelectionOffset = selection.getOffset() + selection.getLength();
+		
+		try {
+			int lineNum= doc.getLineOfOffset(cursorOffset);
+			int numOfLine = doc.getNumberOfLines();
+			
+			if (lineNum == (numOfLine - 2)) {
+				int lastLineOffset = doc.getLineOffset(lineNum+1);
+				editor.selectAndReveal(startSelectionOffset, lastLineOffset - startSelectionOffset);
+				return;
+			}
+			
+			if (lineNum >= (numOfLine - 1)) {
+				editor.selectAndReveal(startSelectionOffset, doc.getLength() - startSelectionOffset );
+				return;
+			}
+			
+			int beginOffset = doc.getLineOffset(lineNum);
+			int endOffset = doc.getLineOffset(lineNum+1) - 1;
+			String currentLine = doc.get(beginOffset, endOffset - beginOffset + 1);
+			boolean isCurrentLineEmpty = currentLine.trim().isEmpty();
+
+			// find next non-empty line which follows an empty line
+			lineNum++;
+			while (lineNum < numOfLine - 1) {
+				beginOffset = doc.getLineOffset(lineNum);
+				endOffset = doc.getLineOffset(lineNum+1) - 1;
+				String nextLine = doc.get(beginOffset, endOffset - beginOffset + 1);
+				if (nextLine.trim().isEmpty()) {
+					isCurrentLineEmpty = true;
+					lineNum++;
+				}
+				else if (!isCurrentLineEmpty)
+					lineNum++;
+				else
+					break;
+			}
+			int newOffset = doc.getLineOffset(lineNum);
+			editor.selectAndReveal(startSelectionOffset, newOffset - startSelectionOffset);
+		} catch (BadLocationException e) {
+			return;
+		}
+	}
+    
+	public void dispose() {
+	}
+
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
